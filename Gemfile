03a73cfbdf73cb43ff5024b0d8fb97aa406a3070
alexsummers <alexsummers@yandex.ua>
2014-05-15 12:18:58 +0300

    Start: created Rails App

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+gem 'pg'
+
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
+# gem 'therubyracer',  platforms: :ruby
+# gem 'turbolinks'
+# gem 'jbuilder', '~> 2.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FormulaEditor::Application.config.secret_key_base = 'f06fdebaacd0fb4e4f3dccd005261ec7385873cc0f5908cd70d3b0c1a061a34334a8fd5f784a89b4db73feaddcb9324e84690ba1c5699be8ca44cf6996e3b188'
